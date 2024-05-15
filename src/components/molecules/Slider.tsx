e5348f3c9e9adc1ccff935b79a018da6156badc1
dilane3 <komboudilane125@gmail.com>
2024-05-15 07:11:12 +0200

    feat: remove all the logic

--- src/components/molecules/Slider.tsx
+++ src/components/molecules/Slider.tsx
@@ -5,73 +5,27 @@ type Props = {
   children: React.ReactNode
 }
 
+const step = 216;
+
 export default function Slider({ children }: Props) {
   // const childrenArray = React.Children.toArray(children)
   const sliderRef = React.useRef<HTMLDivElement>(null);
 
   // Local state
   const [totalWidth, setTotalWidth] = React.useState(0);
-  const [step, setStep] = React.useState(216);
-  const [numberOfSlides, setNumberOfSldes] = React.useState(0);
   const [position, setPosition] = React.useState(0);
 
   useEffect(() => {
-    const slider = sliderRef.current;
-    
-    if (!slider) return;
-    const sliderContent = slider.querySelector(".slider__content");
-
-    // reset the scroll x to 0
-    slider.scrollTo({
-      behavior: "smooth",
-      left: position
-    })
-
-    if (sliderContent) {
-      // get the total width of the slider content
-      setTotalWidth(sliderContent.clientWidth || 0);
-      setNumberOfSldes(sliderContent.children.length);
-    }
+    // TODO: Get the total width of the slider and set it to local state
   }, []);
 
   useEffect(() => {
-    const slider = sliderRef.current;
-    
-    if (!slider) return;
-
-    slider.scrollTo({
-      behavior: "smooth",
-      left: position
-    })
+    // TODO: Add animation on scroll
   }, [position])
 
   // Handlers
   const handleSlide = (action: "next" | "prev") => {
-    const slider = sliderRef.current;
-
-    if (!slider) return;
-
-    const viewerWidth = slider.clientWidth;
-
-    if (action === "next") {
-      const newPosition = position + step;
-      const isLastSlide = newPosition >= totalWidth - viewerWidth;
-
-      if (!isLastSlide) {
-        setPosition(newPosition);
-      } else {
-        setPosition(totalWidth - viewerWidth);
-      }
-    } else {
-      const newPosition = position - step;
-      const isFirstSlide = newPosition <= 0;
-
-      if (!isFirstSlide) {
-        setPosition(newPosition);
-      } else {
-        setPosition(0);
-      }
-    }
+    // TODO: Implement the navigation logic
   }
 
   return (
